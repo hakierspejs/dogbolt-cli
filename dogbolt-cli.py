6b8b14459f05ff9272d44b6eb7d72fc088ee4bc4
Jacek "d33tah" Wielemborek <git@d33.pl>
2025-01-19 16:35:08 +0100

    Introduce download_result(), use argv

--- dogbolt-cli.py
+++ dogbolt-cli.py
@@ -1,9 +1,13 @@
+#!/usr/bin/env python3
+
 import os
 import time
 import hashlib
-import requests
 import json
 import logging
+import argparse
+
+import requests
 
 RETRY_SLEEP = 30
 RETRY_COUNT = 10
@@ -24,14 +28,14 @@ DECOMPILER_NAMES = {
     "Snowman": "snowman",
 }
 
+CACHE_DIR = os.path.expanduser("~/.cache/dogbolt/")
+
+
 def log(msg):
     logging.info(msg)
 
-def main(file_path):
-    if not file_path or not os.path.isfile(file_path):
-        log("Error: Please provide a valid path to the file")
-        exit(1)
 
+def maybe_upload_binary(file_path):
     log(f"binary path: {file_path}")
 
     file_size = os.path.getsize(file_path)
@@ -50,9 +54,8 @@ def main(file_path):
     file_sha256 = compute_sha256(file_path)
     log(f"binary hash: sha256:{file_sha256}")
 
-    CACHE_DIR = os.path.expanduser("~/.cache/dogbolt/")
     binary_id_cache_path = os.path.join(CACHE_DIR, "binary_id.txt")
-    result_hash_cache_path = os.path.join(CACHE_DIR, "result_hash.txt")
+    os.path.join(CACHE_DIR, "result_hash.txt")
     os.makedirs(CACHE_DIR, exist_ok=True)
 
     binary_id = ""
@@ -74,6 +77,83 @@ def main(file_path):
             f.write(f"sha256:{file_sha256} {binary_id}\n")
 
     log(f"binary id: {binary_id}")
+    return binary_id
+
+
+def download_result(
+    result,
+    file_path,
+    done_decompiler_keys,
+    request_count_by_decompiler_key,
+    binary_id,
+):
+    decompiler_name = result["decompiler"]["name"]
+    decompiler_version = result["decompiler"]["version"]
+    decompiler_key = f"{decompiler_name}-{decompiler_version}"
+
+    if decompiler_key in done_decompiler_keys:
+        return
+
+    if USE_DECOMPILER_NAME_MAP and decompiler_name in DECOMPILER_NAMES:
+        decompiler_name = DECOMPILER_NAMES[decompiler_name]
+
+    output_extension = (
+        "c" if decompiler_name != "snowman" else CPP_FILE_EXTENSION
+    )
+    output_path = os.path.join(
+        os.path.dirname(file_path),
+        "src",
+        f"{decompiler_name}-{decompiler_version}",
+        f"{os.path.basename(file_path).rsplit('.', 1)[0]}.{output_extension}",
+    )
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    error = result.get("error")
+    if error == "Exceeded time limit":
+        if (
+            request_count_by_decompiler_key.get(decompiler_key, 0)
+            >= REQUESTS_PER_DECOMPILER
+        ):
+            log(f"error: timeout from decompiler {decompiler_key}")
+            return
+        request_count_by_decompiler_key[decompiler_key] = (
+            request_count_by_decompiler_key.get(decompiler_key, 0) + 1
+        )
+        log(
+            f"error: timeout from decompiler {decompiler_key} - retrying "
+            f"(done {request_count_by_decompiler_key[decompiler_key]} of "
+            f"{REQUESTS_PER_DECOMPILER} requests)"
+        )
+        requests.post(
+            f"https://dogbolt.org/api/binaries/{binary_id}/"
+            f"decompilations/{result['id']}/rerun/"
+        )
+        return
+    elif error:
+        log(f"error: {decompiler_name}-{decompiler_version}")
+        if WRITE_ERROR_TXT:
+            with open(
+                os.path.join(os.path.dirname(output_path), "error.txt"),
+                "w",
+            ) as f:
+                f.write(error)
+        done_decompiler_keys.add(decompiler_key)
+        return
+
+    download_url = result["download_url"]
+    log(f"writing {output_path}")
+    with open(output_path, "wb") as f:
+        f.write(requests.get(download_url).content)
+
+    done_decompiler_keys.add(decompiler_key)
+
+
+def main(file_path):
+    if not file_path or not os.path.isfile(file_path):
+        log("Error: Please provide a valid path to the file")
+        exit(1)
+
+    binary_id = maybe_upload_binary(file_path)
 
     log("fetching decompiler names")
     response = requests.get("https://dogbolt.org/")
@@ -89,87 +169,45 @@ def main(file_path):
     done_decompiler_keys = set()
     request_count_by_decompiler_key = {}
 
-    for retry_step in range(RETRY_COUNT):
+    for _retry_step in range(RETRY_COUNT):
         log("fetching results...")
         response = requests.get(
-            f"https://dogbolt.org/api/binaries/{binary_id}/decompilations/?completed=true"
+            f"https://dogbolt.org/api/binaries/{binary_id}/"
+            "decompilations/?completed=true"
         )
         status_json = response.json()
-        count = status_json["count"]
+        status_json["count"]
 
         for result in status_json["results"]:
-            decompiler_name = result["decompiler"]["name"]
-            decompiler_version = result["decompiler"]["version"]
-            decompiler_key = f"{decompiler_name}-{decompiler_version}"
-
-            if decompiler_key in done_decompiler_keys:
-                continue
-
-            if USE_DECOMPILER_NAME_MAP and decompiler_name in DECOMPILER_NAMES:
-                decompiler_name = DECOMPILER_NAMES[decompiler_name]
-
-            output_extension = (
-                "c" if decompiler_name != "snowman" else CPP_FILE_EXTENSION
+            download_result(
+                result,
+                file_path,
+                done_decompiler_keys,
+                request_count_by_decompiler_key,
+                binary_id,
             )
-            output_path = os.path.join(
-                os.path.dirname(file_path),
-                "src",
-                f"{decompiler_name}-{decompiler_version}",
-                f"{os.path.basename(file_path).rsplit('.', 1)[0]}.{output_extension}",
-            )
-            os.makedirs(os.path.dirname(output_path), exist_ok=True)
-
-            error = result.get("error")
-            if error == "Exceeded time limit":
-                if (
-                    request_count_by_decompiler_key.get(decompiler_key, 0)
-                    >= REQUESTS_PER_DECOMPILER
-                ):
-                    log(f"error: timeout from decompiler {decompiler_key}")
-                    continue
-                request_count_by_decompiler_key[decompiler_key] = (
-                    request_count_by_decompiler_key.get(decompiler_key, 0) + 1
-                )
-                log(
-                    f"error: timeout from decompiler {decompiler_key} - retrying (done {request_count_by_decompiler_key[decompiler_key]} of {REQUESTS_PER_DECOMPILER} requests)"
-                )
-                rerun_response = requests.post(
-                    f"https://dogbolt.org/api/binaries/{binary_id}/decompilations/{result['id']}/rerun/"
-                )
-                continue
-            elif error:
-                log(f"error: {decompiler_name}-{decompiler_version}")
-                if WRITE_ERROR_TXT:
-                    with open(
-                        os.path.join(
-                            os.path.dirname(output_path), "error.txt"
-                        ),
-                        "w",
-                    ) as f:
-                        f.write(error)
-                done_decompiler_keys.add(decompiler_key)
-                continue
-
-            download_url = result["download_url"]
-            log(f"writing {output_path}")
-            with open(output_path, "wb") as f:
-                f.write(requests.get(download_url).content)
-
-            done_decompiler_keys.add(decompiler_key)
 
         if len(done_decompiler_keys) == decompilers_count:
             log("fetched all results")
             break
 
         log(
-            f"fetched {len(done_decompiler_keys)} of {decompilers_count} results. retrying in {RETRY_SLEEP} seconds"
+            f"fetched {len(done_decompiler_keys)} of {decompilers_count}"
+            f" results. retrying in {RETRY_SLEEP} seconds"
         )
         time.sleep(RETRY_SLEEP)
 
     log("The process is complete.")
 
 
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--file-path", type=str, help="Path to the file", required=True
+    )
+    return parser.parse_args().__dict__
+
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    file_path = input("Enter the path to the file: ")
-    main(file_path)
+    main(**parse_args())
